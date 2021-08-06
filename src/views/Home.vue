4b814c390ef8a2771602152ce501e150686fc4b1
程志轩 <czx981194315@gmail.com>
2021-08-06 11:19:10 +0800

    修改23423

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="home">
-        <div><input type="file" @change="fileChange" accept="image/*"><span @click="downLoad()">下载滚滚滚f</span></div>
+        <div><input type="file" @change="fileChange" accept="image/*"><span @click="downLoad()">下载滚滚滚</span></div>
         <div id='initialImg'>
             
         </div>
